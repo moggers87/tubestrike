69739129c6d6ce21cd4bb8d53483634dd62830fc
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2015-08-07 23:51:40 +0100

    Gameloop

--- tubestrike/__init__.py
+++ tubestrike/__init__.py
@@ -1,19 +1,37 @@
 from __future__ import division, print_function, unicode_literals
 
+import sys
+
+from pygame import display
 import pygame
 
+
 def setup():
+    """Setup game environment"""
     width = 800
     height = 600
 
     print("Setting game resolution to {} x {}".format(width, height))
-    screen = pygame.display.set_mode((width, height), pygame.DOUBLEBUF)
-    pygame.display.set_caption("tubestrike!")
+    screen = display.set_mode((width, height), pygame.DOUBLEBUF)
+    display.set_caption("tubestrike!")
 
     screen.fill((0, 0, 0))
-    pygame.display.flip()
+    display.flip()
+
+
+def loop():
+    """Main gameloop"""
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        clock.tick(24)
 
 
 def main():
     setup()
-    input()  # for testing, remove later
+    loop()
