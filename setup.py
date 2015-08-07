57e3fb526ea83e8ec6dffd5a7e8de6d6db6cd208
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2015-08-07 22:18:32 +0100

    Setup.py
    Touch #1

---
+++ setup.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+classify = [
+    "Intended Audience :: End Users/Desktop",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python",
+    "Topic :: Games/Entertainment :: Role-Playing",
+    "Topic :: Games/Entertainment",
+]
+
+setup(
+    classifiers=classify,
+    description="TubeStrike!\n\nAn Endless Commuter",
+    install_requires=["pygame"],
+    license="GPLv3+",
+    name="tubestrike",
+    version="0.0.1"
+)
