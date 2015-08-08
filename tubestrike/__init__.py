aa33720f348e872e7259cc345b97e22e1cbde0d6
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2015-08-08 16:57:49 +0100

    Canvas scaling
    We'll draw onto this canvas and then scale it to fit the display

--- tubestrike/__init__.py
+++ tubestrike/__init__.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function, unicode_literals
 
 import sys
 
-from pygame import display
+from pygame import draw, display
 import pygame
 
 
@@ -22,13 +22,34 @@ def setup():
 def loop():
     """Main gameloop"""
     clock = pygame.time.Clock()
+    screen = display.get_surface()  # the "display"
+    canvas = pygame.Surface((1024, 512))
+    canvas.fill((128, 0, 128))
+
+    block = pygame.Rect(5, 5, 1014, 502)
+    draw.rect(canvas, (255, 255, 255), block)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("Quitting...")
                 pygame.quit()
                 sys.exit()
 
+        screen_res = screen.get_size()
+        screen_ratio = screen_res[0]/screen_res[1]
+        canvas_res = canvas.get_size()
+        canvas_ratio = canvas_res[0]/canvas_res[1]
+
+        if screen_ratio > canvas_ratio:
+            canvas_copy_res = (screen_res[0], int(screen_res[0] / canvas_ratio))
+        else:
+            canvas_copy_res = (int(canvas_ratio * screen_res[1]), screen_res[1])
+
+        canvas_copy = pygame.transform.smoothscale(canvas, canvas_copy_res)
+        screen.blit(canvas_copy, (0, 0))
+
+        display.flip()
         clock.tick(24)
 
 
