7f3bb94b43bc115538d792e215873ecde3a49e06
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2015-08-11 11:33:40 +0100

    Revert "Put event stuff in a separate thread"
    Doesn't work, I think I'll just get rid of resizing for now
    
    This reverts commit 4d68cd79a47ec90ce42b36f72ca25f48cf901fb3.

--- tubestrike/__init__.py
+++ tubestrike/__init__.py
@@ -2,20 +2,25 @@ from __future__ import division, print_function, unicode_literals
 
 from pkg_resources import resource_filename
 import math
+import random
 import sys
-import threading
 
 from pygame import display, transform
 import pygame
 
 
-def setup(width=800, height=600):
+def setup():
     """Setup game environment"""
     display.init()
     pygame.font.init()
 
+    pygame.event.set_allowed(pygame.QUIT)
+
+    width = 800
+    height = 600
+
     print("Setting game resolution to {} x {}".format(width, height))
-    screen = display.set_mode((width, height), pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.RESIZABLE)
+    screen = display.set_mode((width, height), pygame.DOUBLEBUF)
     display.set_caption("tubestrike!")
 
     screen.fill((0, 0, 0))
@@ -50,8 +55,7 @@ def loop():
     canvas = pygame.Surface((640, 200))
     canvas.fill((128, 100, 200))
 
-    pygame.event.set_allowed([pygame.QUIT, pygame.VIDEORESIZE])
-
+    print("Entering main game loop...")
     def render_text(text, colour, antialiasing=True):
         # PyGame 1.9.1 doesn't seem to like file objects
         # TODO: use resource_stream once it works
@@ -60,33 +64,25 @@ def loop():
         title = font.render(text, antialiasing, colour)
         return title
 
-    def event_ticker():
-        while True:
-            event = pygame.event.wait()
-            if event.type == pygame.QUIT:
-                print("Quitting...")
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.VIDEORESIZE:
-                setup(event.dict["w"], event.dict["h"])
-
-    event_thread = threading.Thread(target=event_ticker)
-    event_thread.start()
-
     title_surface = render_text("Tubestrike!", (255, 255, 255))
     title_center = (
         int(canvas.get_size()[0]/2 - title_surface.get_size()[0]/2),
         int(canvas.get_size()[1]/2 - title_surface.get_size()[1]/2),
     )
 
+    c = 0
     zoom_val = 1
     rot_val = 0
     zoom_delta = 0.01
     rot_delta = 0.1
 
-    print("Entering main game loop...")
-    while event_thread.is_alive():
-        pygame.event.pump()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Quitting...")
+                pygame.quit()
+                sys.exit()
+
         title_copy = transform.rotozoom(title_surface, rot_val, zoom_val)
         title_center = (
             int(canvas.get_size()[0]/2 - title_copy.get_size()[0]/2),
@@ -100,6 +96,8 @@ def loop():
         zoom_delta = (zoom_delta + 0.005) % 1
         rot_delta = (rot_delta + 0.01) % 2
 
+        if c > 0:
+            c = c - 1
         paint_canvas_onto_screen(screen, canvas)
         display.flip()
         clock.tick(24)
