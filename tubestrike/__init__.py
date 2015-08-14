7263efeb4e12be376a31b13e66a11fe63d985862
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2015-08-14 11:39:07 +0100

    Add tracks to bottom of screen

--- tubestrike/__init__.py
+++ tubestrike/__init__.py
@@ -5,7 +5,7 @@ import math
 import random
 import sys
 
-from pygame import display, transform
+from pygame import display, draw, transform
 import pygame
 
 
@@ -25,6 +25,25 @@ def setup():
 
     screen.fill((0, 0, 0))
     display.flip()
+
+
+def make_track_surface():
+    """Paint some tracks onto a surface"""
+    track_shape = (640, 5)
+    track_shadow = (640, 3)
+    sleeper = (10, 40)
+    surf = pygame.Surface((640, 40), flags=pygame.SRCALPHA)
+    # sleepers
+    for i in xrange(5, 640, 20):
+        draw.rect(surf, (178, 150, 250), pygame.Rect((i, 0), sleeper))
+
+    # tracks
+    draw.rect(surf, (178, 150, 250), pygame.Rect((0, 2), track_shape))
+    draw.rect(surf, (153, 125, 225), pygame.Rect((0, 7), track_shadow))
+    draw.rect(surf, (178, 150, 250), pygame.Rect((0, 27), track_shape))
+    draw.rect(surf, (153, 125, 225), pygame.Rect((0, 32), track_shadow))
+
+    return surf
 
 
 def paint_canvas_onto_screen(screen, canvas):
@@ -55,27 +74,24 @@ def loop():
     canvas = pygame.Surface((640, 200))
     canvas.fill((128, 100, 200))
 
-    print("Entering main game loop...")
-    def render_text(text, colour, antialiasing=True):
-        # PyGame 1.9.1 doesn't seem to like file objects
-        # TODO: use resource_stream once it works
-        font = pygame.font.Font(resource_filename("tubestrike", "assets/fonts/Hammersmith_One/HammersmithOne-Regular.ttf"), 40)
+    # PyGame 1.9.1 doesn't seem to like file objects
+    # TODO: use resource_stream once it works
+    font = pygame.font.Font(resource_filename("tubestrike", "assets/fonts/Hammersmith_One/HammersmithOne-Regular.ttf"), 40)
 
-        title = font.render(text, antialiasing, colour)
-        return title
-
-    title_surface = render_text("Tubestrike!", (255, 255, 255))
+    title_surface = font.render("Tubestrike!", True, (255, 255, 255))
     title_center = (
         int(canvas.get_size()[0]/2 - title_surface.get_size()[0]/2),
         int(canvas.get_size()[1]/2 - title_surface.get_size()[1]/2),
     )
 
-    c = 0
+    track_surface = make_track_surface()
+
     zoom_val = 1
     rot_val = 0
     zoom_delta = 0.01
     rot_delta = 0.1
 
+    print("Entering main game loop...")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -83,12 +99,14 @@ def loop():
                 pygame.quit()
                 sys.exit()
 
+        canvas.fill((128, 100, 200))
+        canvas.blit(track_surface, (0, 148))
+
         title_copy = transform.rotozoom(title_surface, rot_val, zoom_val)
         title_center = (
             int(canvas.get_size()[0]/2 - title_copy.get_size()[0]/2),
             int(canvas.get_size()[1]/2 - title_copy.get_size()[1]/2),
         )
-        canvas.fill((128, 100, 200))
         canvas.blit(title_copy, title_center)
 
         rot_val = 15 * math.sin((math.pi * rot_delta) + 2)
@@ -96,8 +114,6 @@ def loop():
         zoom_delta = (zoom_delta + 0.005) % 1
         rot_delta = (rot_delta + 0.01) % 2
 
-        if c > 0:
-            c = c - 1
         paint_canvas_onto_screen(screen, canvas)
         display.flip()
         clock.tick(24)
