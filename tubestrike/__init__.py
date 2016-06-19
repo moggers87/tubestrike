fb177f7a35737f6709a753ed3d9adc11799222ca
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-06-19 18:12:12 +0100

    Transition
    Currently only transitions to another menu :)

--- tubestrike/__init__.py
+++ tubestrike/__init__.py
@@ -86,15 +86,21 @@ def paint_canvas_onto_screen(screen, canvas):
 
 
 class Menu(object):
-    def __init__(self):
+    def __init__(self, track=None):
         self.canvas = pygame.Surface((640, 200), flags=pygame.HWSURFACE)
 
         # PyGame 1.9.1 doesn't seem to like file objects
         # TODO: use resource_stream once it works
         title_font = pygame.font.Font(resource_filename("tubestrike", "assets/fonts/Hammersmith_One/HammersmithOne-Regular.ttf"), 40)
+        subtitle_font = pygame.font.Font(resource_filename("tubestrike", "assets/fonts/Roboto_Mono/RobotoMono-Regular.ttf"), 15)
 
         self.title_surface = title_font.render("Tubestrike!", True, (255, 255, 255))
-        self.track_surface = make_track_surface()
+        self.subtitle_surface = subtitle_font.render("PRESS SPACE TO START", True, (255, 255, 255))
+
+        if track is None:
+            self.track_surface = make_track_surface()
+        else:
+            self.track_surface = track
 
         self.zoom_val = 1
         self.rot_val = 0
@@ -118,6 +124,10 @@ class Menu(object):
             int(self.canvas.get_size()[0]/2 - title_shadow_copy.get_size()[0]/2),
             int(self.canvas.get_size()[1]/3 - title_shadow_copy.get_size()[1]/2),
         )
+        subtitle_center = (
+            int(self.canvas.get_size()[0]/2 - self.subtitle_surface.get_size()[0]/2),
+            int((2*self.canvas.get_size()[1]/3) - self.subtitle_surface.get_size()[1]/2),
+        )
 
         # shadow effect
         pixels_alpha = pygame.surfarray.pixels_alpha(title_shadow_copy)
@@ -126,11 +136,54 @@ class Menu(object):
 
         self.canvas.blit(title_shadow_copy, title_shadow_center)
         self.canvas.blit(title_copy, title_center)
+        self.canvas.blit(self.subtitle_surface, subtitle_center)
 
         self.rot_val = 15 * math.sin((math.pi * self.rot_delta) + 2)
         self.zoom_val = math.sin(math.pi * self.zoom_delta) + 0.5
         self.zoom_delta = (self.zoom_delta + 0.0039) % 1
         self.rot_delta = (self.rot_delta + 0.008) % 2
+
+    def event_KEYDOWN(self, event):
+        if event.key == pygame.K_q:
+            pygame.event.post(pygame.event.Event(pygame.QUIT))
+        elif event.key == pygame.K_SPACE:
+            # XXX: should transition to actual game
+            self.next_scene = MoveOver(self, Menu(self.track_surface))
+
+
+class MoveOver(object):
+    def __init__(self, from_scene, to_scene):
+        self.canvas = pygame.Surface((640, 200), flags=pygame.HWSURFACE)
+        self.track_surface = from_scene.track_surface
+        self.remains = from_scene.canvas.get_size()[0]
+
+        self.from_scene = from_scene
+        self.to_scene = to_scene
+
+        self.next_scene = self
+
+    def render(self):
+        self.canvas.fill((128, 100, 200))
+        if self.remains > 0 and self.from_scene is not None:
+            self.canvas.blit(self.track_surface, (self.remains, 148))
+            self.from_scene.render()
+
+            self.canvas.blit(self.from_scene.canvas, ((self.remains - self.from_scene.canvas.get_size()[0]), 0))
+            self.remains -= 3
+        elif self.remains > 0 and self.from_scene is None:
+            self.canvas.blit(self.to_scene.canvas, (self.remains, 0))
+            self.to_scene.render()
+
+            self.canvas.blit(self.track_surface, (self.remains - self.to_scene.canvas.get_size()[0], 148))
+            self.remains -= 3
+        elif self.from_scene is not None:
+            self.canvas.blit(self.track_surface, (self.remains, 148))
+            self.from_scene = None
+            self.remains = self.to_scene.canvas.get_size()[0] + self.remains
+        else:
+            self.canvas = self.to_scene.canvas
+            self.to_scene.render()
+            self.next_scene = self.to_scene
 
     def event_KEYDOWN(self, event):
         if event.key == pygame.K_q:
